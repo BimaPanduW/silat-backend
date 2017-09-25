ff6a722ac6eca1c2f4ea6623a1af96eac0fc8051
Bima Pandu Wiguna <bima.p@go-jek.com>
2017-09-25 21:09:12 +0700

    [Bima/Cesar] Added BaseDAO to Create Table

--- src/main/java/com/bima/silat/managers/DataBaseManager.java
+++ src/main/java/com/bima/silat/managers/DataBaseManager.java
@@ -1,5 +1,6 @@
 package com.bima.silat.managers;
 
+import com.bima.silat.persistence.BaseDAO;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
@@ -15,19 +16,10 @@ public class DataBaseManager {
 
         DBI dbi = new DBI(mysqlDataSource);
 
-        Handle handle = dbi.open();
-        handle.execute("create table users (" +
-                "id int primary key, " +
-                "name VARCHAR(50), " +
-                "password VARCHAR(10), " +
-                "role VARCHAR(10), " +
-                "email varchar(50), " +
-                "phone_number VARCHAR(50), " +
-                "date_created DATETIME, " +
-                "date_updated DATETIME, " +
-                "is_active BIT" +
-                ")");
+        BaseDAO baseDAO = dbi.open(BaseDAO.class);
 
-        handle.close();
+        baseDAO.createUserTable();
+
+        baseDAO.close();
     }
 }
